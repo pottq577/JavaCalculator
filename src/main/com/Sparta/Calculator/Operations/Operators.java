e26ee8706271d81710c59609572903a1da0a95b3
HWKIM <pottq577@naver.com>
2025-03-04 18:25:29 +0900

    enum 람다식 표현 사용

--- src/main/com/Sparta/Calculator/Operations/Operators.java
+++ src/main/com/Sparta/Calculator/Operations/Operators.java
@@ -1,53 +1,29 @@
 package main.com.Sparta.Calculator.Operations;
 
 public enum Operators implements Computable {
-  ADD("+") {
-    @Override
-    public double compute(double value1, double value2) {
-      return value1 + value2;
+  ADD("+", (value1, value2) -> value1 + value2),
+  SUBTRACT("-", (value1, value2) -> value1 - value2),
+  MULTIPLY("*", (value1, value2) -> value1 * value2),
+  DIVIDE("/", (value1, value2) -> {
+    if (value2 == 0) {
+      throw new ArithmeticException("0으로 나눌 수 없습니다.");
     }
-  },
-  DIVIDE("/") {
-    @Override
-    public double compute(double value1, double value2) {
-      if (value2 == 0) {
-        throw new ArithmeticException("0으로 나눌 수 없습니다.");
-      }
-      return value1 / value2;
+    return value1 / value2;
+  }),
+  MODULAR("%", (value1, value2) -> {
+    if (value2 == 0) {
+      throw new ArithmeticException("0으로 나눌 수 없습니다.");
     }
-  },
-  MODULO("%") {
-    @Override
-    public double compute(double value1, double value2) {
-      if (value2 == 0) {
-        throw new ArithmeticException("0으로 나눌 수 없습니다.");
-      }
-      return value1 % value2;
-    }
-  },
-  MULTIPLY("*") {
-    @Override
-    public double compute(double value1, double value2) {
-      return value1 * value2;
-    }
-  },
-  POWER("^") {
-    @Override
-    public double compute(double value1, double value2) {
-      return Math.pow(value1, value2);
-    }
-  },
-  SUBTRACT("-") {
-    @Override
-    public double compute(double value1, double value2) {
-      return value1 - value2;
-    }
-  };
+    return value1 % value2;
+  }),
+  POWER("^", (value1, value2) -> Math.pow(value1, value2));
 
   private final String symbol;
+  private final Computable computation;
 
-  Operators(String symbol) {
+  Operators(String symbol, Computable computation) {
     this.symbol = symbol;
+    this.computation = computation;
   }
 
   public static Operators fromSymbol(String symbol) {
@@ -60,5 +36,7 @@ public enum Operators implements Computable {
     throw new IllegalArgumentException("지원하지 않는 연산자입니다: " + symbol);
   }
 
-  public abstract double compute(double value1, double value2);
+  public double compute(double value1, double value2) {
+    return computation.compute(value1, value2);
+  }
 }
